1c1ccff66b50af6f4c7a1d351a1faf0cee26b9a2
DariuszCedro <dariuszcedro@gmail.com>
2023-08-05 21:17:04 +0200

    task-09

--- js/task-04.js
+++ js/task-04.js
@@ -3,7 +3,7 @@ let counterValue = 0;
 const buttonDec = document.querySelector(`button[data-action="decrement"]`);
 const buttonInc = document.querySelector(`button[data-action="increment"]`);
 
-let showCounter = document.querySelector("#value");
+const showCounter = document.querySelector("#value");
 
 buttonDec.addEventListener("click", () => {
   counterValue -= 1;
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector("button");
+const text = document.querySelector("span.color");
+
+const handler = (e) => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = `${randomColor}`;
+  text.innerHTML = `${randomColor}`;
+  //console.log(randomColor);
+};
+
+button.addEventListener("click", handler);
