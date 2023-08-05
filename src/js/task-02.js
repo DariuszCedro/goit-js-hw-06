72fc46da668246f2ef7b042b3c678e0ad7d433ac
DariuszCedro <dariuszcedro@gmail.com>
2023-08-05 12:21:09 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+  const elementOfIngredients = document.createElement("li");
+  elementOfIngredients.textContent = ingredients[i];
+  elementOfIngredients.classList.add("item");
+  list.append(elementOfIngredients);
+}
